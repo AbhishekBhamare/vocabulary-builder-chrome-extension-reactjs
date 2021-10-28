0fb57875a2294a024f6734b0f32eae5bb7060ff6
AbhishekBhamare <66770654+AbhishekBhamare@users.noreply.github.com>
2021-10-28 23:45:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import Divider from '@material-ui/core/Divider'
 import { WORD_LIST } from './WordList';
@@ -6,76 +6,60 @@ import { KEY } from './Key';
 import './App.css';
 
 function App() {
-  const [newData, setNewData] = useState([]);
-  // let temp=0, key, x, data;
-  // const getRandomKey = () => {
-  //   temp = Math.floor(Math.random() * KEY.length);
-  // }
+  let temp=0, key, x, data;
+  const getRandomKey = () => {
+    temp = Math.floor(Math.random() * KEY.length);
+  }
 
-  // let arr = [];
-  // const getRandomValue = () => {
-  //   getRandomKey();
-  //   key = KEY[temp];
-  //   console.log(KEY[temp]);
-  //   for(let i in WORD_LIST[0][key]){
-  //     arr.push(i);
-  //   }
-  // }
-
-  // const getValue = () => {
-  //   getRandomValue();
-  //   const temp = Math.floor(Math.random() * arr.length)
-  //   x = arr[temp];
-  // }
-  // let eg = '';
-  // const getData = () => {
-  //   getValue();
-  //   data = WORD_LIST[0][key][x];
-  //   x = x.toUpperCase(); 
-  //   console.log(data) 
-  //   eg = data['meaning_1'];
-  //   eg.charAt(0).toUpperCase();  
-  //   console.log("this-",eg);
-  //   console.log("ottis",data['meaning_1']);
-  // }
-  // getData();
-  let arr = [],rnd1,rnd2,key,val,eg,data;
-  useEffect(() => {
-    arr = [];
-    rnd1 = Math.floor(Math.random() * KEY.length);
-    key = KEY[rnd1];
-    console.log(KEY[rnd1]);
+  let arr = [];
+  const getRandomValue = () => {
+    getRandomKey();
+    key = KEY[temp];
+    console.log(KEY[temp]);
     for(let i in WORD_LIST[0][key]){
       arr.push(i);
     }
+  }
 
-    rnd2 = Math.floor(Math.random() * arr.length);
-    val = arr[rnd2];
-    eg = '';
-    data = WORD_LIST[0][key][val];
-    val = val.toUpperCase();
-    setNewData(data);
-  }, [newData]);
+  const getValue = () => {
+    getRandomValue();
+    const temp = Math.floor(Math.random() * arr.length)
+    x = arr[temp];
+  }
+  let eg = '';
+  const getData = () => {
+    getValue();
+    data = WORD_LIST[0][key][x];
+    x = x.toUpperCase(); 
+    console.log(data) 
+    eg = data['meaning_1'];
+    eg.charAt(0).toUpperCase();  
+    console.log("this-",eg);
+    console.log("ottis",data['meaning_1']);
+    
+  }
+  getData();
 
-  // const load = event => {
-  //   event.preventDefault();
-  //   window.location.reload();
-  // }
+
+  const load = event => {
+    event.preventDefault();
+    window.location.reload();
+  }
   return (
     <React.Fragment>
      <div className="modal">
-       <h1>{val}</h1>
+       <h1>{x}</h1>
        <h2>{eg}</h2>
        <Divider/>
        <h2>MNEMONICS</h2>
-       <div className="p"><p>{newData['mnemonic_1']}</p></div>
-       <p>{newData['mnemonic_2']}</p>
-       <p>{newData['mnemonic_3']}</p>
-       <p>{newData['mnemonic_4']}</p>
-     </div>
-     {/* <div className="btn">
+       <div className="p"><p>{data['mnemonic_1']}</p></div>
+       <p>{data['mnemonic_2']}</p>
+       <p>{data['mnemonic_3']}</p>
+       <p>{data['mnemonic_4']}</p>
+       <div className="btn">
          <button onClick={load}>Next</button>
-     </div> */}
+     </div>
+     </div>
     </React.Fragment>
   );
 }
